4cca1c26ff413779968c3bc1337ca5a00c7996a2
John Paul <kimenjuivy@gmail.com>
2025-11-02 23:56:58 +0300

    add new changes to allow vercel implementation

--- server/server.js
+++ server/server.js
@@ -12,11 +12,15 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://mern-stack-integration-kimenjuivy-snowy.vercel.app'],
+  origin: [
+    'http://localhost:3000',
+    'https://mern-stack-integration-kimenjuivy.vercel.app' // 👈 correct domain
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
